11235e5d2223c236fdd317a97ae27bad4123f26b
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-09 13:58:49 +0200

    :art: Refactor image adding code

--- canvas.js
+++ canvas.js
@@ -1,40 +1,43 @@
+const state = {
+  firstImage: true
+};
+
 export function setup(canvas) {
   const context = canvas.getContext('2d');
-  const img = document.createElement('img');
-  img.addEventListener('load', onImageLoad);
 
-  canvas.addEventListener('dragover', onContextDragOver);
-  canvas.addEventListener('drop', onContextDrop);
-
-  function onImageLoad() {
-    clearCanvas(canvas);
-    context.drawImage(img, 0, 0);
-  }
-
-  function onContextDrop(event) {
+  canvas.addEventListener('drop', event => {
     const {files} = event.dataTransfer;
     if (files.length > 0) {
       const file = files[0];
       if (file.type.includes('image')) {
         const reader = new FileReader();
-        reader.addEventListener('load', onReaderLoad);
+        const img = document.createElement('img');
+
+        img.addEventListener('load', () => {
+          clearCanvas(canvas);
+          if (state.firstImage) {
+            state.firstImage = false;
+            return context.drawImage(img, 0, 0);
+          }
+          return context.drawImage(img, 50, 50);
+        });
+
+        reader.addEventListener('load', event => {
+          img.src = event.target.result;
+        });
+
         reader.readAsDataURL(file);
+
         event.preventDefault();
       }
     }
+  });
 
-    function onReaderLoad(event) {
-      img.src = event.target.result;
-    }
-  }
+  canvas.addEventListener('dragover', event => event.preventDefault());
 }
 
 function clearCanvas(canvas) {
   const context = canvas.getContext('2d');
   context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function onContextDragOver(event) {
-  event.preventDefault();
 }
 
