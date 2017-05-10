c8e7d4204126049e0bf6075391435413c29dd5f6
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-10 17:08:10 +0200

    Add powerzone radius to map (#15)
    * :sparkles: Replace canvas with Leaflet map
    
    * :sparkles: replace demo map with clean version
    
    * :sparkles: init map with icons
    
    * :bug: increase default zoomlevel
    
    * :sparkles: add powerzone radius
    
    * :bug: remove git stuff
    
    * :sparkles: add two extra powerzones

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 export default function initMap(element) {
   const map = L.map('map', {
-    crs: L.CRS.Simple
+    crs: L.CRS.Simple,
+    minZoom: 1
   });
 
   const bounds = [[0, 0], [200, 400]];
@@ -16,6 +17,14 @@ export default function initMap(element) {
   const iconEnergy = new marker({iconUrl: '/img/icon-energy.svg'});
   const iconFood = new marker({iconUrl: '/img/icon-food.svg'});
 
+  // Radius around powerzone 3
+  L.circle([100, 200], {
+    color: 'grey',
+    fillColor: 'grey',
+    fillOpacity: 0.3,
+    radius: 50
+  }).addTo(map);
+
   L.marker([100, 200], {icon: iconEnergy}).addTo(map).bindPopup(`
   <h3>Powerzone #3</h3>
   <dl>
@@ -26,8 +35,44 @@ export default function initMap(element) {
     <dd>Type B (1)</dd>
   </dl>
   `);
-  L.marker([150, 75], {icon: iconFood}).addTo(map).bindPopup('I\'m another marker');
 
+  // Radius powerzone 2
+  L.circle([150, 310], {
+    color: 'grey',
+    fillColor: 'grey',
+    fillOpacity: 0.3,
+    radius: 50
+  }).addTo(map);
+
+  L.marker([150, 310], {icon: iconEnergy}).addTo(map).bindPopup(`
+  <h3>Powerzone #2</h3>
+  <dl>
+    <dt>Verbruik</dt>
+    <dd>Overkill</dd>
+    <dt>Generatoren</dt>
+    <dd>Type A (2)</dd>
+  </dl>
+  `);
+
+  // Radius powerzone 1
+  L.circle([50, 320], {
+    color: 'grey',
+    fillColor: 'grey',
+    fillOpacity: 0.3,
+    radius: 50
+  }).addTo(map);
+
+  L.marker([50, 320], {icon: iconEnergy}).addTo(map).bindPopup(`
+  <h3>Powerzone #1</h3>
+  <dl>
+    <dt>Verbruik</dt>
+    <dd>Matig</dd>
+    <dt>Generatoren</dt>
+    <dd>Type D (3)</dd>
+  </dl>
+  `);
+
+  L.marker([150, 75], {icon: iconFood}).addTo(map).bindPopup('I\'m another marker');
 
   map.fitBounds(bounds);
 }
