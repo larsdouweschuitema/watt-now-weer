6734abefc8711cbd5429c7e446feefd9371c70ea
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-11 16:45:20 +0200

    :construction: add edit view

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import {setup as setupCharts} from './charts.js';
 import initMap from './map.js';
 
-setupCharts(document.querySelector('charts'));
+if (document.body.classList.contains('index')) {
+  setupCharts(document.querySelector('charts'));
+}
+
 initMap(document.getElementById('map'));
